776d4db4e9f4d00e88b9088f8f79da607a3ad7d3
Michael <mjdaly10@gmail.com>
2019-10-18 17:05:15 -0700

    Renamed PF_TILE_WIDTH to TILE_WIDTH

--- src/core/constants.js
+++ src/core/constants.js
@@ -3,9 +3,10 @@ const CANVAS_HEIGHT = 226;
 
 const PIXEL_WIDTH = 2;  // Pixels' width for Atari 2600 are double their height.
 
-const PF_TILE_WIDTH    = 4 * PIXEL_WIDTH;
+const TILE_WIDTH = 4 * PIXEL_WIDTH;
+
 const PF_WIDTH_TILES   = 36;
-const PF_WIDTH_PIXELS  = PF_WIDTH_TILES * PF_TILE_WIDTH;
+const PF_WIDTH_PIXELS  = PF_WIDTH_TILES * TILE_WIDTH;
 const PF_HEIGHT_PIXELS = 192;
 
 const MAX_COLORS = 128;
@@ -18,7 +19,8 @@ const constants =
 
 	PIXEL_WIDTH,
 
-	PF_TILE_WIDTH,
+	TILE_WIDTH,
+
 	PF_WIDTH_TILES,
 	PF_WIDTH_PIXELS,
 	PF_HEIGHT_PIXELS,

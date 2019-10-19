e2c2e2aec0d97d4a6e59ad98b16e4465450c2579
Michael <mjdaly10@gmail.com>
2019-10-19 01:40:18 -0700

    Added documentation to drawFillRect() function

--- src/utility/fillDraw.js
+++ src/utility/fillDraw.js
@@ -1,3 +1,13 @@
+/**
+ * Draw a filled rectangle on a canvas context.
+ *
+ * @param {CanvasRenderingContext2D} context
+ * @param {string}                   color   - An RGBA string separated by commas.
+ * @param {integer}                  x
+ * @param {integer}                  y
+ * @param {integer}                  width
+ * @param {integer}                  height
+ */
 const drawFillRect = ( context, color, x, y, width, height ) =>
 {
 	context.fillStyle = `rgba(${color})`;

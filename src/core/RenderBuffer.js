571ca59eda56ac29aa39d705ee55203059f89f83
Michael <mjdaly10@gmail.com>
2019-10-26 01:48:05 -0700

    Added coordinate checks for drawPixel()

--- src/core/RenderBuffer.js
+++ src/core/RenderBuffer.js
@@ -18,6 +18,11 @@ class RenderBuffer
 
 	drawPixel ( x, y, colorArray )
 	{
+		if ( x < 0  ||  y < 0  ||  x >= this.width  ||  y >= this.height )
+		{
+			return;
+		}
+
 		setPixel (this.imageData, x, y, colorArray);
 	}
 

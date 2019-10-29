bd8d7808ec8d41fc1cab4be5cea0c44ec173bec3
Michael <mjdaly10@gmail.com>
2019-10-29 16:08:49 -0700

    Added ValueLinker utility class

---
+++ src/utility/classes/ValueLinker.js
@@ -0,0 +1,51 @@
+import SetMap from '~/utility/classes/SetMap.js';
+
+
+/**
+ * A data structure used for linking two values together.
+ */
+class ValueLinker
+{
+	constructor ()
+	{
+		this.values = new SetMap ();
+	}
+
+	/**
+	 * Link two values to each other.
+	 *
+	 * @param {*} value1
+	 * @param {*} value2
+	 */
+	link ( value1, value2 )
+	{
+		this.values.add (value1, value2);
+		this.values.add (value2, value1);
+	}
+
+	/**
+	 * Unlink two values from each other.
+	 *
+	 * @param {*} value1
+	 * @param {*} value2
+	 */
+	unlink ( value1, value2 )
+	{
+		this.values.delete (value1, value2);
+		this.values.delete (value2, value1);
+	}
+
+	/**
+	 * Check if two values are linked to each other.
+	 *
+	 * @param {*} value1
+	 * @param {*} value2
+	 */
+	linked ( value1, value2 )
+	{
+		return this.values.has (value1, value2)  &&  this.values.has (value2, value1);
+	}
+}
+
+
+export default ValueLinker;

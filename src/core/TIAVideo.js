d7eb84b10c50cdc57fe615aa076bf1bb5f34ce5f
Michael <mjdaly10@gmail.com>
2019-10-30 00:46:39 -0700

    Temporary pitstop before I change things again

--- src/core/TIAVideo.js
+++ src/core/TIAVideo.js
@@ -1,11 +1,15 @@
-import RenderBuffer from '~/core/RenderBuffer.js';
-import EventEmitter from '~/utility/classes/EventEmitter.js';
-import Playfield    from '~/playfield/Playfield.js';
-import Player       from '~/player/Player.js';
-import MissileBall  from '~/missileBall/MissileBall.js';
+import has from 'has';
 
-import { getColor }      from '~/palettes/palettes.js';
-import { CANVAS_HEIGHT } from '~/core/constants.js';
+import TIACollision  from '~/core/TIACollision.js';
+import EventEmitter  from '~/utility/classes/EventEmitter.js';
+import Playfield     from '~/playfield/Playfield.js';
+import Player        from '~/player/Player.js';
+import MissileBall   from '~/missileBall/MissileBall.js';
+
+import { coordToTile } from '~/utility/snapCoord.js';
+import { getColor }    from '~/palettes/palettes.js';
+
+import { TILE_WIDTH } from '~/playfield/constants.js';
 
 
 /**
@@ -18,14 +22,72 @@ class TIAVideo
 	 */
 	constructor ( palette = 'NTSC' )
 	{
-		this.palette   = palette;
-		this.events    = new EventEmitter ();
-		this.playfield = new Playfield ();
-		this.player0   = new Player ();
-		this.player1   = new Player ();
-		this.ball      = new MissileBall ();
-		this.missile0  = new MissileBall ();
-		this.missile1  = new MissileBall ();
+		this.palette      = palette;
+		this.events       = new EventEmitter ();
+		this.tiaCollision = new TIACollision ();
+		this.playfield    = new Playfield ();
+		this.player0      = new Player ();
+		this.player1      = new Player ();
+		this.ball         = new MissileBall ();
+		this.missile0     = new MissileBall ();
+		this.missile1     = new MissileBall ();
+		this.scanline     = 0;
+		this.pixel        = 0;
+	}
+
+	renderPlayfield ( renderBuffer )
+	{
+		const { playfield, tiaCollision, scanline, pixel, palette } = this;
+
+		const tileX = coordToTile (pixel, TILE_WIDTH);
+		const tile  = playfield.getTile (tileX);
+
+		if ( tile === -1 )
+		{
+			return;
+		}
+
+		const colorRGBA = getColor (palette, tile ? playfield.tileColor : playfield.backgroundColor);
+
+		renderBuffer.drawPixel (pixel, scanline, colorRGBA);
+
+		if ( tile === 1 )
+		{
+			tiaCollision.addObjectToPixel ('playfield');
+		}
+	}
+
+	renderBall ( renderBuffer )
+	{
+		const { playfield, ball, tiaCollision, scanline, pixel, palette } = this;
+
+		if ( !ball.isRenderCoord (pixel)  ||  !ball.enabled )
+		{
+			return;
+		}
+
+		renderBuffer.drawPixel (pixel, scanline, getColor (palette, playfield.tileColor));
+
+		tiaCollision.addObjectToPixel ('ball');
+	}
+
+	renderPlayer ( renderBuffer, playerKey )
+	{
+		const player = this[playerKey];
+
+		if ( !(player instanceof Player) )
+		{
+			return;
+		}
+
+		const { tiaCollision, scanline, pixel, palette } = this;
+
+		if ( player.coordToPixel (pixel) === 1 )
+		{
+			renderBuffer.drawPixel (pixel, scanline, getColor (palette, player.color));
+
+			tiaCollision.addObjectToPixel (playerKey);
+		}
 	}
 
 	/**
@@ -34,20 +96,39 @@ class TIAVideo
 	 */
 	render ( renderBuffer )
 	{
-		const { events, palette, playfield, player0, player1, ball, missile0, missile1 } = this;
+		const { events, palette } = this;
+		const { tiaCollision, playfield, player0, player1, ball, missile0, missile1 } = this;
 
-		for ( let scanline = 0;  scanline < CANVAS_HEIGHT;  scanline++ )
+		// Fallback color if nothing gets drawn.
+		const baseColor = getColor (palette, 0);
+
+		this.scanline = 0;
+
+		// Scanline-by-scanline.
+		for ( let scanline = 0;  scanline < renderBuffer.height;  scanline++ )
 		{
+			// Set the scanline property rather than directly modify it every time so it looks cleaner.
+			this.scanline = scanline;
+
 			events.emit ('scanline', scanline);
 
-			playfield.render (renderBuffer, palette, scanline);
-			ball.render (renderBuffer, getColor (palette, playfield.tileColor), scanline);
+			this.pixel = 0;
 
-			player0.render (renderBuffer, palette, scanline);
-			missile0.render (renderBuffer, getColor (palette, playfield.tileColor), scanline);
+			// Pixel-by-pixel.
+			for ( let pixel = 0;  pixel < renderBuffer.width;  pixel++ )
+			{
+				this.pixel = pixel;
 
-			player1.render (renderBuffer, palette, scanline);
-			missile1.render (renderBuffer, getColor (palette, playfield.tileColor), scanline);
+				renderBuffer.drawPixel (pixel, scanline, baseColor);
+
+				this.renderPlayfield (renderBuffer);
+				this.renderBall (renderBuffer);
+				this.renderPlayer (renderBuffer, 'player1');
+				this.renderPlayer (renderBuffer, 'player0');
+
+				tiaCollision.setPixelCollisions ();
+				tiaCollision.clearPixelObjects ();
+			}
 		}
 
 		return renderBuffer.imageData;
